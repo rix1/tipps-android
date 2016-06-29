2acd4b6103618bc48ec412b5017b589787244ef2
killcam <camiberggren@gmail.com>
2016-06-29 16:25:53 +0200

    master work

--- app/src/main/java/no/tipps/tipps/Vipps.java
+++ app/src/main/java/no/tipps/tipps/Vipps.java
@@ -63,19 +63,6 @@ public class Vipps extends AppCompatActivity {
                 ));
             }
         });
-
-        // Floating button with email icon
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
-                Intent intent = new Intent(getApplicationContext(), PaymentActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
     @Override
